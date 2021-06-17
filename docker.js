3fefef82c2980964410efa2da4eda98419e71978
Abhijeet Kumar Singh <1398Aaabhisingh911@gmail.com>
2021-06-17 10:36:39 +0530

    Modularised code

--- docker.js
+++ docker.js
@@ -1,1 +1,20 @@
 //action='http://192.168.218.6/cgi-bin/docker.py
+
+function lw1(e) {
+  e.preventDefault();
+}
+
+function lw2(f) {
+  f.preventDefault();
+  alert("The form2 was submitted");
+}
+
+function lw3(g) {
+  g.preventDefault();
+  alert("The form3 was submitted");
+}
+
+function lw4(h) {
+  h.preventDefault();
+  alert("The form4 was submitted");
+}
